ada62eea925b717897217d2ea10f3058e4676b38
Darius Bagdonavicius <dariusbag@gmail.com>
2016-02-27 09:24:58 +0200

    Functions isolated and placed to separate directory in file functions.php 3 functions: findMin(); findMax(); findMinMax(); File task04.php updated

---
+++ functions/functions.php
@@ -0,0 +1,36 @@
+﻿<?php
+function findMin($ar){
+// takes array of numbers
+// returns the smallest number
+    $minimum = $ar[0];
+    foreach($ar as $i){
+        if($i <= $minimum){
+            $minimum = $i;
+        }
+    }
+    return $minimum;
+}
+
+function findMax($ar){
+// takes array of numbers
+// returns the biggest number
+    $maximum = $ar[0];
+    foreach($ar as $i){
+        if($i >= $maximum){
+            $maximum = $i;
+        }
+    }
+    return $maximum;
+}
+
+function findMinMax($ar){
+// takes array of numbers
+// returns minimum and maximum in array
+    $min = findMin($ar);
+    $max = findMax($ar);
+    $minMax = [$min, $max];
+    return $minMax;
+}
+
+?>
+
--- task04.php
+++ task04.php
@@ -1,4 +1,5 @@
 ﻿<?php
+include 'functions/functions.php';
 $arr = [0, 89, 2, -52];
 
 $minMax = findMinMax($arr);
@@ -7,35 +8,4 @@ foreach($minMax as $i){
     echo $i .'<br />';
 }
 
-function findMin($ar){
-// finding minimumm in array
-    $minimum = $ar[0];
-    foreach($ar as $i){
-        if($i <= $minimum){
-            $minimum = $i;
-        }
-    }
-    return $minimum;
-}
-
-function findMax($ar){
-// finding maximum in array
-    $maximum = $ar[0];
-    foreach($ar as $i){
-        if($i >= $maximum){
-            $maximum = $i;
-        }
-    }
-    return $maximum;
-}
-
-function findMinMax($ar){
-// returns minimum and maximum in array
-    $min = findMin($ar);
-    $max = findMax($ar);
-    $minMax = [$min, $max];
-    return $minMax;
-}
-
 ?>
-
